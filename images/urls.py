4fe0c68cd5396e586147cfcf3985e348f8008716
Lygerius <stanislav_shubin@mail.ru>
2021-03-10 00:12:55 +0300

    5

--- images/urls.py
+++ images/urls.py
@@ -6,7 +6,6 @@ from .views import ImageViewSet
 
 urlpatterns = [
     path("", ImageViewSet.as_view(), name="image_list"),
-    path('upload', ImageViewSet.as_view(), name='upload-file'),
 ]+static(settings.STATIC_URL,
          document_root=settings.STATIC_ROOT
          )+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
